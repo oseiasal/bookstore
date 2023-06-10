e85324c03cd0705b51a08b1d532c7277abcfc275
Oseias Nascimento <39315752+oseiasal@users.noreply.github.com>
2023-06-10 19:08:27 +0000

    add many

--- order/serializers/order_serializer.py
+++ order/serializers/order_serializer.py
@@ -4,7 +4,7 @@ from product.models import Product
 from product.serializers.product_serializer import ProductSerializer
 
 class OrderSerializer(serializers.ModelSerializer):
-    product = ProductSerializer(required=True)
+    product = ProductSerializer(required=True, many=True)
     total = serializers.SerializerMethodField()
 
     def get_total(self, instance):
