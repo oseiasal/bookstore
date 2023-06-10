b2bbbc37e9f0ccc719f848fc2c39cc94d11c3006
Oseias Nascimento <39315752+oseiasal@users.noreply.github.com>
2023-06-10 19:03:50 +0000

    inclusão dos serializers

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for bookstore project.
+"""bookstore URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path, re_path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    re_path("bookstore/(?P<version>(v1|v2))/", include("order.urls")),
+    re_path("bookstore/(?P<version>(v1|v2))/", include("product.urls")),
 ]
