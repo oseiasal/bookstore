1301fe3accdca2b0030119fa77d25fac34d8256c
Oseias Nascimento <39315752+oseiasal@users.noreply.github.com>
2023-06-11 04:12:05 +0000

    add testes

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -20,6 +20,6 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # re_path("bookstore/(?P<version>(v1|v2))/", include("order.urls")),
+    re_path("bookstore/(?P<version>(v1|v2))/", include("order.urls")),
     re_path("bookstore/(?P<version>(v1|v2))/", include("product.urls")),
 ]
