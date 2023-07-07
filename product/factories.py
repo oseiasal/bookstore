f23490175881758c9afd8dea5122d8a366f0c549
Oseias Nascimento <oseiasal@gmail.com>
2023-07-06 22:52:21 -0300

    fix: erro nos testes

--- product/factories.py
+++ product/factories.py
@@ -1,7 +1,7 @@
 import factory
 
-from product.models import Product
-from product.models import Category
+from product.models import Category, Product
+
 
 class CategoryFactory(factory.django.DjangoModelFactory):
     title = factory.Faker("pystr")
@@ -12,16 +12,17 @@ class CategoryFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Category
 
+
 class ProductFactory(factory.django.DjangoModelFactory):
-    price = factory.Faker('pyint')
+    price = factory.Faker("pyint")
     category = factory.LazyAttribute(CategoryFactory)
-    title = factory.Faker('pystr')
+    title = factory.Faker("pystr")
 
     @factory.post_generation
     def category(self, create, extracted, **kwargs):
         if not create:
             return
-        
+
         if extracted:
             for category in extracted:
                 self.category.add(category)
--- product/models/product.py
+++ product/models/product.py
@@ -1,6 +1,7 @@
 from django.db import models
 
-from product.models.category import Category
+from product.models import Category
+
 
 class Product(models.Model):
     title = models.CharField(max_length=100)
@@ -8,3 +9,7 @@ class Product(models.Model):
     price = models.PositiveIntegerField(null=True)
     active = models.BooleanField(default=True)
     category = models.ManyToManyField(Category, blank=True)
+
+
+    def __str__(self):
+        return self.title
