615863fec5df51e3897b7d9bf9e2bb746de645b3
Oseias Nascimento <39315752+oseiasal@users.noreply.github.com>
2023-07-08 12:05:28 -0300

    GitHub actions (#8)
    * feat: add workflows
    
    * add python version
    
    * fix: python 3.7
    
    * fix: python 3.1 bug
    
    * remover configuração de autenticação
    
    * ajustes nos arquivos
    
    * fix: erro nos testes
    
    * fix: pull request aguardando para testar

--- product/viewsets/product_viewset.py
+++ product/viewsets/product_viewset.py
@@ -1,14 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.authentication import BasicAuthentication, SessionAuthentication, TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
 
 from product.models import Product
 from product.serializers.product_serializer import ProductSerializer
 
 
 class ProductViewSet(ModelViewSet):
-    authentication_classes = [BasicAuthentication,SessionAuthentication, TokenAuthentication]
-    permission_classes = [IsAuthenticated]
     serializer_class = ProductSerializer
 
     def get_queryset(self):
